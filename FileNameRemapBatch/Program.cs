b572d33770105f2c3fa91de9a6abd71cde36bea2
Gino Messmer <g.messmer@ftsoftware.eu>
2017-03-07 10:39:35 +0100

    File remap

--- FileNameRemapBatch/Program.cs
+++ FileNameRemapBatch/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace FileNameRemapBatch
 {
-    class Program
+    public class Program
     {
+        public static Dictionary<string, string> ReplaceMap = new Dictionary<string, string>()
+        {
+            // old -> new
+            { "ü", "ue" },
+            { "ä", "ae" },
+            { "ö", "oe" }
+        };
+
         static void Main(string[] args)
         {
+            WriteLine("File directory to fix:");
+            string path = ReadLine();
+
+            // Check whether directory exists
+            if (Directory.Exists(path))
+            {
+                // Get all files
+                var files = Directory.GetFiles(path).ToList();
+
+                foreach (var file in files)
+                {
+                    FileInfo currentFile = new FileInfo(file);
+                    foreach (var map in ReplaceMap)
+                    {
+                        // Check whether name contains chars in remap source
+                        if (currentFile.Name.Contains(map.Key))
+                        {
+                            // Assign new name
+                            string newName = currentFile.Name.Replace(map.Key, map.Value);
+                            string newPath = $"{currentFile.DirectoryName}\\{newName}";
+
+                            // Do it
+                            WriteLine($"{DateTime.Now.ToShortTimeString()}: Renaming <{currentFile.Name}> to <{newPath}>");
+                            currentFile.MoveTo(newPath);
+                        }
+                    }
+                }
+
+                ForegroundColor = ConsoleColor.Green;
+                WriteLine("All files have been successfully renamed. Press any key to exit...");
+            }
+            else
+            {
+                // Directory doesn't exist -> restart
+                ForegroundColor = ConsoleColor.Red;
+                WriteLine($"<{path}> doesn't exist. Please restart this application and try again.");
+            }
+
+            // Exit
+            Read();
         }
     }
 }
